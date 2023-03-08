0fb69ec93e06cd3805b6fdf27c2588362f500cf5
K0rte <tothvili9@gmail.com>
2023-03-08 17:28:11 +0100

    mouseover works

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (let c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
+
+const squares = document.querySelectorAll(".grid-item")
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => square.style.backgroundColor ="green")
+});
